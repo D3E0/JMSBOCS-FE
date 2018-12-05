2dc173da9841a7c1275cf7a26f35851810b95838
yu <914179030@qq.com>
2018-12-05 14:02:31 +0800

    init

--- src/components/qiniu.vue
+++ src/components/qiniu.vue
@@ -1,34 +1,19 @@
 <template>
     <el-form ref="qiniuForm" :model="milk" label-width="90px" style="width: 500px">
         <el-form-item label="Access Key" prop="ak">
-            <el-input v-model="milk.ak" type="password"></el-input>
+            <el-input v-model="milk.ak" type="text"></el-input>
         </el-form-item>
         <el-form-item label="Secret Key" prop="sk">
-            <el-input v-model="milk.sk" type="password"></el-input>
+            <el-input v-model="milk.sk" type="text"></el-input>
         </el-form-item>
-        <el-form-item label="Bucket">
-            <el-select v-model="milk.bucket" placeholder="请输入 Bucket"
-                       style="width: 100%"
-                       filterable
-                       remote
-                       :remote-method="remoteMethod"
-                       :loading="loading"
-                       prop="bucket">
-                <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value"
-                        :disabled="item.disabled">
-                </el-option>
-            </el-select>
+        <el-form-item label="Bucket" prop="bucket">
+            <el-input v-model="milk.bucket" type="text"></el-input>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">立即修改</el-button>
             <el-button @click="resetForm('qiniuForm')">取消</el-button>
         </el-form-item>
     </el-form>
-
 </template>
 
 <script>
@@ -41,43 +26,20 @@
                     sk: "",
                     bucket: "",
                 },
-                options: [{value: "请先填写 AK, SK", disabled: true}],
-                loading: false,
             };
         },
         methods: {
-            remoteMethod() {
-                if (this.milk.ak !== '' && this.milk.sk !== '') {
-                    setTimeout(this.fetchData(), 200);
-                } else {
-                    this.options = [{value: "请先填写 AK, SK", disabled: true}];
-                }
-            },
-            fetchData() {
-                this.loading = true;
-                this.axios.get('/api/qiniu', {
-                    params: {
-                        accessKey: this.$root.uid,
-                        secretKey: this.$root.uid,
-                    }
-                }).then(response => {
-                    this.loading = false;
-                    let reg = new RegExp("[\\[\\]\"]", "g");
-                    let list = response.data.data.replace(reg, '').split(',');
-                    this.options = list.map(item => {
-                        return {value: item, label: item};
-                    });
-                }).catch(error => {
-                    this.$message.error(error);
-                });
-            },
             onSubmit() {
+                if (this.milk.ak === '' || this.milk.sk === '' || this.milk.bucket === '') {
+                    this.$message.error("请先填写 AK, SK, Bucket");
+                    return;
+                }
                 const params = new URLSearchParams();
                 params.append('id', this.$root.uid);
                 params.append('accessKey', this.milk.ak);
                 params.append('secretKey', this.milk.sk);
                 params.append('bucket', this.milk.bucket);
-                this.axios.post('/api/user/qiniu', params).then(response => {
+                this.axios.post('/api/qiniu', params).then(response => {
                     if (response.data.message === 'success') {
                         this.$message({
                             message: '修改成功',
